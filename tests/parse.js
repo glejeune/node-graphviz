b3decbe35153690b9c6dc0f386ccdeadc07b2530
glejeune <gregoire.lejeune@free.fr>
2010-10-05 18:22:04 +0200

    Major bugs corrections Update Attributs class

--- tests/parse.js
+++ tests/parse.js
@@ -8,5 +8,13 @@ graphviz.parse( "cluster.dot", function(graph) {
 graphviz.parse( "cluster.dot", function(graph) {
 	graph.render( "png", function(render) {
 		process.stdout.write( render );
+	}, function(code, out, err) {
+		console.log("RENDER ERROR")
+		console.log(""+err)
+		console.log(""+out)
 	});
+}, function(code, out, err) {
+	console.log("PARSE ERROR")
+	console.log(""+err)
+	console.log(""+out)
 });
--- tests/server/app.js
+++ tests/server/app.js
@@ -1,14 +1,14 @@
 var express = require('express'),
-	app = express.createServer(),
-	temp = require('temp'),
-	fs = require('fs');
+  app = express.createServer(),
+  temp = require('temp'),
+  fs = require('fs');
 
 var sys = require('sys'),
-	graphviz = require('graphviz');
+  graphviz = require('../../lib/graphviz');
 
 app.set('views', __dirname + '/views');
 app.configure(function(){ 
-	app.use(express.bodyDecoder());
+  app.use(express.bodyDecoder());
 })
 
 app.get('/', function(req, res){
@@ -16,17 +16,27 @@ app.get('/', function(req, res){
 });
 
 app.post('/test', function(req,res){
-	temp.open('dotGraph', function(err, info) {
-	  fs.write(info.fd, req.body.data);
-	  fs.close(info.fd, function(err) {
-			graphviz.parse( info.path, function(graph) {
-				graph.render( "png", function(render) {
-					img = '<img src="data:image/png;base64,'+render.toString("base64")+'"/>'
-					res.send(img)
-				});
-			});
-	  });
-	});
+  temp.open('dotGraph', function(err, info) {
+    fs.write(info.fd, req.body.data);
+    fs.close(info.fd, function(err) {
+      graphviz.parse( info.path, function(graph) {
+        graph.render( "png", function(render) {
+          img = '<img src="data:image/png;base64,'+render.toString("base64")+'"/>'
+          res.send(img)
+        }, function(code, out, err) {
+          img = '<div class="error"><p><b>Render error (code '+code+')</b></p>';
+          img += '<p>STDOUT : '+out+'</p>';
+          img += '<p>STDERR : '+err+'</p></div>';
+          res.send(img)
+        });
+      }, function(code, out, err){
+        img = '<div class="error"><p><b>Parser error (code '+code+')</b></p>';
+        img += '<p>STDERR : '+err+'</p></div>';
+        img += '<p>STDOUT : '+out+'</p></div>';
+        res.send(img)
+      });
+    });
+  });
 })
 
 app.listen(3000);
