8db289b6f1f2cd819307635a93d2cff3597e53dc
glejeune <gregoire.lejeune@gmail.com>
2012-08-04 18:15:46 +0200

    Issue #10

--- Jakefile.js
+++ Jakefile.js
@@ -6,13 +6,13 @@ var util = require('util'),
 
 var docTitle = 'node-graphviz'
 var docFiles = 'lib/graphviz.js lib/deps/graph.js lib/deps/node.js lib/deps/edge.js'
-var outputDocFile = 'documentation.html'
+var outputDocFile = 'documentation.json'
 var docRibbon = 'http://github.com/glejeune/node-graphviz'
 var docDesc = '[Node.js](http://nodejs.org) interface to the [GraphViz](http://graphviz.org) graphing tool'
 
 desc('Generate node-graphviz documentation.');
 task('doc', [], function () {
-  child = exec('dox -r ' + docRibbon + ' -d "' + docDesc + '" -t "' + docTitle + '" -J ' + docFiles + ' > ' + outputDocFile, 
+  child = exec('dox -r ' + docRibbon + ' -d "' + docDesc + '" -t "' + docTitle + '" < ' + docFiles + ' > ' + outputDocFile, 
     function (error, stdout, stderr) {
       if (error !== null) {
         console.log('exec error: ' + error);
